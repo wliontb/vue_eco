4ea54140d33f284323805e9491db9a94ff480e50
anhnvt <anhnguyenviettuan@admicro.vn>
2023-06-22 17:31:50 +0700

    init project

---
+++ components/AppFooter.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="bg-gray-400 p-5 w-4/6 mx-auto mt-5 rounded-t-lg">
+        <div class="flex justify-center">
+            <div class="flex w-2/6">
+                <IconLetter class="text-white" />
+                <div class="text-white">ĐĂNG KÝ NHẬN BẢN TIN</div>
+            </div>
+            <div class="p-1 bg-white rounded-lg w-3/6 flex">
+                <input type="text" placeholder="Nhập địa chỉ email của bạn" class="w-full">
+                <button class="bg-orange-500 text-white rounded-lg py-2 px-5">Đăng ký</button>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white w-4/6 mx-auto flex pt-5">
+        <div class="w-2/6">
+            <img src="img/logo.png" alt="">
+            <div>
+                <p>
+                    Lầu 5, 387-389 Hai Bà Trưng Quận 3 TP HCMCông Ty Cổ Phần Phát Hành Sách TP HCM - FAHASA60 - 62 Lê Lợi,
+                    Quận 1, TP. HCM, Việt Nam
+                </p>
+                <p>
+                    Fahasa.com nhận đặt hàng trực tuyến và giao hàng tận nơi. KHÔNG hỗ trợ đặt mua và nhận hàng trực tiếp
+                    tại văn phòng cũng như tất cả Hệ Thống Fahasa trên toàn quốc.
+                </p>
+            </div>
+            <img src="img/logocongthuong.png" class="w-2/6" alt="">
+            <div class="flex">
+                <IconSymbol />
+                <IconSymbol />
+                <IconSymbol />
+            </div>
+        </div>
+        <div class="w-4/6 border-l border-gray-400">
+            <div class="flex">
+                <div class="w-2/6">
+                    DỊCH VỤ
+                    <ul>
+                        <li>Điều khoản sử dụng</li>
+                        <li>Chính sách bảo mật thông tin cá nhân</li>
+                        <li>Chính sách bảo mật thanh toán</li>
+                        <li>Giới thiệu</li>
+                        <li>Hệ thống trung tâm - nhà sách</li>
+                    </ul>
+                </div>
+                <div class="w-2/6">
+                    HỖ TRỢ
+                    <ul>
+                        <li>Chính sách đổi - trả - hoàn tiền</li>
+                        <li>Chính sách bảo hành - bồi hoàn</li>
+                        <li>Chính sách vận chuyển</li>
+                        <li>Chính sách khách sỉ</li>
+                        <li>Phương thức thanh toán và xuất HĐ</li>
+                    </ul>
+                </div>
+                <div class="w-2/6">
+                    TÀI KHOẢN CỦA TÔI
+                    <ul>
+                        <li>Đăng nhập/Tạo mới tài khoản</li>
+                        <li>Thay đổi địa chỉ khách hàng</li>
+                        <li>Chi tiết tài khoản</li>
+                        <li>Lịch sử mua hàng</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                LIÊN HỆ
+                <div class="flex">
+                    <div class="w-2/6 flex">
+                        <IconMapPin />
+                        60-62 Lê Lợi, Q.1, TP.HCM
+                    </div>
+                    <div class="w-2/6 flex">
+                        <IconLetter />
+                        cskh@fahasa.com.vn
+                    </div>
+                    <div class="w-2/6 flex">
+                        <IconPhone />
+                        1900636467
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="flex">
+                    <div class="w-1/5">
+                        <img src="img/transport/vnpost1.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/transport/ahamove_logo3.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/transport/icon_giao_hang_nhanh1.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/transport/icon_snappy1.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/transport/Logo_ninjavan.png" alt="">
+                    </div>
+                </div>
+                <div class="flex">
+                    <div class="w-1/5">
+                        <img src="img/payment/vnpay_logo.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/payment/ZaloPay-logo-130x83.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/payment/momopay.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/payment/shopeepay_logo.png" alt="">
+                    </div>
+                    <div class="w-1/5">
+                        <img src="img/payment/logo_moca_120.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="text-gray-300 w-4/6 mx-auto bg-white text-center pt-5">
+        Giấy chứng nhận Đăng ký Kinh doanh số 0304132047 do Sở Kế hoạch và Đầu tư Thành phố Hồ Chí Minh cấp ngày 20/12/2005,
+        đăng ký thay đổi lần thứ 10, ngày 20/05/2022.
+    </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-gray-100">
+        <AppHeader />
+        <slot />
+
+        <AppFooter />
+    </div>
+</template>
+  

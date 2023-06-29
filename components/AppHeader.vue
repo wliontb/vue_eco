e0f0a981492cfb12061ab05c44cb7fa907166a7c
Tuan Anh Nguyen <ntak.ictu@gmail.com>
2023-06-30 00:08:50 +0700

    add code 30/6

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -33,34 +33,87 @@
           <IconGlass />
         </button>
       </div>
-      <div class="flex justify-between items-center px-1">
+      <div class="flex justify-between items-center px-1 text-sm">
         <div class="flex flex-col items-center text-gray-800 mr-2">
           <IconBell />
-          <div class="font-semibold">Thông báo</div>
+          <div>Thông báo</div>
         </div>
         <NuxtLink to="/cart">
-          <div class="flex flex-col items-center text-gray-800 mr-2">
+          <div class="flex flex-col items-center text-gray-800 mr-2 text-sm">
             <IconCart />
-            <div class="font-semibold">
+            <div>
               Giỏ hàng
             </div>
           </div>
         </NuxtLink>
 
 
-        <div class="flex flex-col items-center text-gray-800 mr-2">
-          <IconUser />
-          <div class="font-semibold">Tài khoản</div>
+        <div class="relative">
+          <div class="group" @mouseenter="isHoveringAccount = false" @mouseleave="isHoveringAccount = true">
+            <div class="flex flex-col items-center text-gray-800 mr-2 text-sm">
+              <IconUser class="font-semibold" />
+              <div>Tài khoản</div>
+            </div>
+            <div class="flex flex-col absolute bg-white top-full right-0 px-4 py-2 w-60 rounded-b shadow-sm border-t"
+              :class="{ hidden: isHoveringAccount }">
+              <button class="bg-red-700 text-white px-4 py-2 my-2 rounded-lg text-center font-semibold"
+                @click="displayLoginBox">Đăng nhập</button>
+              <button class="border-2 font-semibold border-red-700 text-red-700 px-4 py-2 rounded-lg text-center">Đăng
+                ký</button>
+            </div>
+          </div>
+
 
         </div>
-        <IconLanguage class="text-gray-800" />
+        <IconLanguage class="text-gray-800 text-sm" />
       </div>
+    </div>
+  </div>
+  <div class="w-full h-full fixed top-0 left-0 z-50 bg-gray-800 bg-opacity-50 flex items-center justify-center"
+    :class="{ hidden: !isDisplayLoginBox }" @click="closeLoginBox">
+    <div class="bg-white p-4 mx-auto w-4/12 flex flex-col rounded-md" @click.stop>
+      <div class="flex">
+        <div class="w-1/2 text-red-700 text-center border-b-2 border-red-700 pb-2 font-semibold">Đăng nhập</div>
+        <div class="w-1/2 text-center">Đăng ký</div>
+      </div>
+      <div class="p-4 flex flex-col">
+        <div class="flex flex-col mb-4">
+          <div>Số điện thoại/Email</div>
+          <input type="text" class="border rounded-sm px-4 py-2" placeholder="Nhập số điện thoại hoặc email">
+        </div>
+        <div class="flex flex-col mb-4">
+          <div>Mật khẩu</div>
+          <input type="password" class="border rounded-sm px-4 py-2" placeholder="Nhập mật khẩu">
+        </div>
+        <div class="text-right mb-4">
+          <NuxtLink to="#" class="text-red-700 text-sm">Quên mật khẩu?</NuxtLink>
+        </div>
+        <button class="px-4 py-2 text-gray-700 bg-gray-200 font-semibold text-center w-3/5 rounded-md mx-auto mb-3">Đăng nhập</button>
+        <button class="px-4 py-2 text-red-700 bg-white border-red-700 border-2 font-semibold text-center w-3/5 rounded-md mx-auto mb-3">Bỏ qua</button>
+        <button class="px-4 py-2 text-white bg-blue-500 font-semibold text-center w-3/5 rounded-md mx-auto mb-3 flex justify-center items-center">
+          <IconFacebook class="w-5 mr-2" />
+          Đăng nhập bằng Facebook
+        </button>
+      </div>
+
     </div>
   </div>
 </template>
 <script setup>
 import { ref } from 'vue';
-import IconBar from './IconBar.vue';
 
 const isHovering = ref(false);
+
+const isHoveringAccount = ref(true);
+
+const isDisplayLoginBox = ref(false);
+
+const displayLoginBox = () => {
+  isDisplayLoginBox.value = true;
+};
+
+const closeLoginBox = () => {
+  isDisplayLoginBox.value = false;
+};
+
 </script>
